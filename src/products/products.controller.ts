89cf815acc569ad7472b9458973f38b98af2f653
VovaSv <vovasvidi@gmail.com>
2023-05-07 18:22:41 +0300

    Controllers

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,18 +1,50 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Query,
+  Body,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
   // GET /products --> []
   @Get()
-  getProducts() {
-    return 'All products';
+  getProducts(@Query('type') type: string) {
+    return [{ type }];
   }
   // GET /products/:id --> {...}
   @Get(':id')
-  getOneProduct() {
-    return [];
+  getOneProduct(@Param('id') id: string) {
+    return { id };
   }
   // POST /products
+  @Post()
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return {
+      name: createProductDto.name,
+    };
+  }
   // PUT /products/:id --> {...}
+  @Put()
+  updateProduct(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return {
+      id,
+    };
+  }
   // DELETE /products/:id
+  @Delete(':id')
+  removeProductProduct(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
 }
